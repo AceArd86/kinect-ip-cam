b4463917876b6e32445089c6ef418b6d7686a7f4
Dickard <122964205+AceArd86@users.noreply.github.com>
2025-08-13 22:10:48 -0500

    Create setenv.example.ps1

---
+++ setenv.example.ps1
@@ -0,0 +1,3 @@
+# Example – edit values then run in PowerShell
+[System.Environment]::SetEnvironmentVariable("KINECTCAM_USER","kinect","User")
+[System.Environment]::SetEnvironmentVariable("KINECTCAM_PASS","change-me","User")
